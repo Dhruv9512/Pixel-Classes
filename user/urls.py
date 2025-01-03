77e1943a4f752ed66cac41dc1625260c834c4ce6
Dhruv9512 <dhruvsharma56780@gmail.com>
2025-01-03 21:44:32 +0530

    Changes in util

--- user/urls.py
+++ user/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ResendOTPView, LoginView, RegisterView, VerifyOTPView
 
 urlpatterns = [
-    path('resend-otp/', ResendOTPView.as_view(), name='resend_otp'),
+    # path('resend-otp/', ResendOTPView.as_view(), name='resend_otp'),
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
-    # path('verify-otp/', VerifyOTPView.as_view(), name='verify_otp'),
+    path('verify-otp/', VerifyOTPView.as_view(), name='verify_otp'),
 ]
