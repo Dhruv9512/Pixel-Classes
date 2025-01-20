2e77697cdb151d24db050b989885e5c2ca3bcf0b
man-navlakha <dhruvsharma56780@gmail.com>
2025-01-21 00:55:57 +0530

    urls changes

--- Home/models.py
+++ Home/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class CourseList(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255 , unique=True , null=False)
