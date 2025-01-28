7d334acfcbe3eac54bc38d58c5f709661cbee136
man-navlakha <dhruvsharma56780@gmail.com>
2025-01-28 21:08:20 +0530

    Create QuePdf table

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import CourseList
+from .models import CourseList , QuePdf
 # Register your models here.
+
+# Registering the model CourseList
 @admin.register(CourseList)
 class CourseListAdmin(admin.ModelAdmin):
     list_display = ('id','name')
+
+# Registering the model QuePdf
+class QuePdfAdmin(admin.ModelAdmin):
+    list_display = ('id','course','pdf','sem')
