14cfe370aeb57aacdf8c9f9177fff095319c9a95
man-navlakha <dhruvsharma56780@gmail.com>
2025-01-29 19:22:41 +0530

    register QuePdf

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import PasswordResetToken
+from .models import PasswordResetToken , QuePdf
 # Register your models here.
+
+# Registering the model PasswordResetToken
 @admin.register(PasswordResetToken)
 class PasswordResetToken(admin.ModelAdmin):
     list_display = ('user', 'is_verified', 'is_reset', 'expiry_date', 'is_expired')
+
+# Registering the model QuePdf Admin
+@admin.register(QuePdf)
+class QuePdfAdmin(admin.ModelAdmin):
+    list_display = ('id','course','pdf','sem')
     
