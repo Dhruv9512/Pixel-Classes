8eb9f3c06517369f1af532ae1aefc09744a9998f
man-navlakha <dhruvsharma56780@gmail.com>
2025-07-15 17:47:40 +0530

    adding details,posts,delete,edit in profile

--- Profile/urls.py
+++ Profile/urls.py
@@ -4,6 +4,6 @@ from .views import ProfileDetailsView,UserPostDeleteView, userPostsView,EditProf
 urlpatterns = [
     path('details/', ProfileDetailsView.as_view(),name='profile_details'),
     path('posts/', userPostsView.as_view(), name='user_posts'),
-    path('delete/', UserPostDeleteView.as_view(), name='user_post_delete'),
+    path('deletePost/', UserPostDeleteView.as_view(), name='user_post_delete'),
     path('edit/', EditProfileView.as_view(), name='edit_profile'),
 ]   
