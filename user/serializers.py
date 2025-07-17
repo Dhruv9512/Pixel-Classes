782afff57e0b9dd2311c2eb8d3d0f6b9ab472c09
man-navlakha <dhruvsharma56780@gmail.com>
2025-07-18 00:22:17 +0530

    solve probleam in signup

--- user/serializers.py
+++ user/serializers.py
@@ -11,7 +11,7 @@ class LoginSerializer(serializers.Serializer):
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password','id','profile_pic']
+        fields = ['username', 'email', 'password','id']
 
     def create(self, validated_data):
         user = User.objects.create_user(
