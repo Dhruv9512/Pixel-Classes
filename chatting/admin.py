a279497f5e1430934151229e7ad411c54088e708
man-navlakha <dhruvsharma56780@gmail.com>
2025-08-08 17:54:49 +0530

    adding send email for unseen msg

--- chatting/admin.py
+++ chatting/admin.py
@@ -3,6 +3,6 @@ from .models import Message
 # Register your models here.
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('sender', 'receiver', 'content', 'timestamp')
+    list_display = ('sender', 'receiver', 'content', 'timestamp','is_seen','seen_at')
     search_fields = ('sender__username', 'receiver__username', 'content')
     list_filter = ('timestamp',)
