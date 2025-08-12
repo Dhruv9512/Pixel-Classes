c5bf830dcd1077d7802b1f11e7fdf754218d7d07
man-navlakha <dhruvsharma56780@gmail.com>
2025-08-12 16:50:20 +0530

    adding date and time

--- chatting/models.py
+++ chatting/models.py
@@ -7,15 +7,15 @@ import pytz
 # Function to return the current time as a string in HH:MM:SS format
 def get_current_datetime():
     ist = pytz.timezone('Asia/Kolkata')
-    return datetime.now(ist)
+    return datetime.now(ist).strftime("%Y-%m-%d %I:%M %p")
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
-    timestamp = models.DateTimeField(default=get_current_datetime)
+    timestamp = models.CharField(default=get_current_datetime)
     is_seen = models.BooleanField(default=False)
-    seen_at = models.DateTimeField(default=get_current_datetime)
+    seen_at = models.CharField(default=get_current_datetime)
 
     def __str__(self):
         return f"{self.sender} -> {self.receiver}: {self.content}"
--- chatting/serializers.py
+++ chatting/serializers.py
@@ -6,14 +6,6 @@ from django.contrib.auth.models import User
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    timestamp = serializers.SerializerMethodField()
-    seen_at = serializers.SerializerMethodField()
-
-    def get_timestamp(self, obj):
-        return obj.timestamp.astimezone(pytz.timezone("Asia/Kolkata")).strftime("%Y-%m-%d %I:%M %p")
-
-    def get_seen_at(self, obj):
-        return obj.seen_at.astimezone(pytz.timezone("Asia/Kolkata")).strftime("%Y-%m-%d %I:%M %p")
 
     class Meta:
         model = Message
