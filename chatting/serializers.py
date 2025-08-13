cbfcf418a43681c0ccf9f4fcb823858417da5708
man-navlakha <dhruvsharma56780@gmail.com>
2025-08-13 13:47:30 +0530

    fix new

--- chatting/serializers.py
+++ chatting/serializers.py
@@ -1,13 +1,18 @@
-import pytz
 from rest_framework import serializers
 from .models import Message
-from django.contrib.auth.models import User
-
-
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.CharField(source='sender.username', read_only=True)
+    receiver = serializers.CharField(source='receiver.username', read_only=True)
 
     class Meta:
         model = Message
-        fields = '__all__'
-
+        fields = [
+            'id',
+            'sender',
+            'receiver',
+            'content',
+            'timestamp',
+            'is_seen',
+            'seen_at'
+        ]
